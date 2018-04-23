dce98c664922fc59ecb42bda2039b2078e7f82ed
Vladimir Novikov <oxore@protonmail.com>
2018-04-24 01:33:59 +0300

    Introduce game on-off state machine

--- snake_xenzia.js
+++ snake_xenzia.js
@@ -21,6 +21,7 @@
             rect.setAttribute("data-count", 0);
             rect.setAttribute("fill", "#ebedf0");
             rightColumn.appendChild(rect);
+            rects[rects.length] = rect;
             rightColumnSize = rightColumn.children.length;
         }
     }
@@ -30,34 +31,61 @@
             rects[i].style.display = "none";
     }
 
+    function showAll(rects) {
+        for (var i = 0; i < rects.length; i++)
+            rects[i].style.display = "block";
+    }
+
     function duplicateAll(rects) {
         var fakes = [];
         for (var i = 0; i < rects.length; i++) {
             fakes[i] = rects[i].cloneNode();
-            fakes[i].className.baseVal = "day2";
+            fakes[i].className.baseVal = "dayfake";
             rects[i].parentElement.appendChild(fakes[i]);
         }
         return fakes;
     }
 
-    function drawStartButton(activityGraph) {
-        var startButtonDiv = document.createElement("div");
+    function createStartButton() {
         var startButton = document.createElement("Button");
         var startButtonLabel = document.createTextNode("Play Snake");
-        startButtonDiv.style.clear = "left";
-        startButtonDiv.appendChild(startButton);
         startButton.appendChild(startButtonLabel);
         startButton.classList.add("btn");
+        startButton.id = "startButton";
+        return startButton;
+    }
+
+    function drawStartButton(startButton, activityGraph) {
+        var startButtonDiv = document.createElement("div");
+        startButtonDiv.style.clear = "left";
+        startButtonDiv.appendChild(startButton);
         var footer = activityGraph[0].getElementsByClassName("contrib-footer clearfix mt-1 mx-3 px-3 pb-1");
         footer[0].appendChild(startButtonDiv);
     }
 
+    function switchGameState() {
+        if (gameState == "off") {
+            hideAll(realRects);
+            showAll(fakeRects);
+            startButton.innerText = "Turn Off";
+            gameState = "on";
+        } else {
+            hideAll(fakeRects);
+            showAll(realRects);
+            startButton.innerText = "Play Snake";
+            gameState = "off";
+        }
+    }
+
     var activityGraph = document.getElementsByClassName("js-contribution-graph");
     if (activityGraph.length > 0) {
-        drawStartButton(activityGraph);
+        var gameState = "off";
+        var startButton = createStartButton();
+        drawStartButton(startButton, activityGraph);
+        startButton.addEventListener("click", switchGameState);
         var realRects = document.getElementsByClassName("day");
         var fakeRects = duplicateAll(realRects);
-        hideAll(realRects);
+        hideAll(fakeRects);
         completeField(fakeRects);
     }
 })();
